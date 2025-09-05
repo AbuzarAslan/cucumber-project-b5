7cd8a3c89e2e049fe6f4eaec7cb0017f31972854
abuza <abuzar.aslanov@gmail.com>
2025-09-04 23:11:11 -0400

    day 1

---
+++ src/test/java/io/loop/utilities/GeneralConstants.java
@@ -0,0 +1,19 @@
+package io.loop.utilities;
+
+public class GeneralConstants {
+
+    //browser types
+
+    public static final String CHROME = "chrome";
+    public static final String FIREFOX = "firefox";
+    public static final String SAFARI = "safari";
+
+
+    public static final String HEROKUAPP_URL = "https://the-internet.herokuapp.com/forgot_password";
+
+
+
+
+
+
+}
